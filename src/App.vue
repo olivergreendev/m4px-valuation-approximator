cb6d72b3016d65de597f3a2f323fcdbd832d108b
Oliver Green <82689255+olivergreendev@users.noreply.github.com>
2022-06-22 20:38:18 +0100

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PriceCalculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PriceCalculator from './components/PriceCalculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PriceCalculator
+}
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+*,
+::before,
+::after {
+    box-sizing: border-box;
+}
+body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f7f9;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 0;
+  margin: 0;
+}
+input {
+  font-family: inherit;
+  padding: 10px;
+  border-radius: 6px;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  transition: box-shadow 0.1s ease;
+}
+input:focus {
+  box-shadow: 0 0 1px 1px rgba(0, 122, 255);
 }
 </style>
---
+++ src/components/PriceCalculator.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="column pp">
+                <div class="row">
+                    <div class="row-text">PP</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_pp">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">PPx1.2</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_pp_12">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">PPx1.3</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_pp_13">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">PPx1.4</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_pp_14" step="0.01">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">PPx1.5</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_pp_15">
+                    </div>
+                </div>
+            </div>
+            <div class="column mu">
+                <div class="row">
+                    <div class="row-text">RETAIL (R)</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_retail">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">MU</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_mu_1">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">MU</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_mu_2">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">MU</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_mu_3">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-text">MU</div>
+                    <div class="row-box">
+                        <input type="number" v-model="text_mu_4">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PriceCalculator',
+    data () {
+        return {
+            text_pp: 0,
+            text_pp_12: 0,
+            text_pp_13: 0,
+            text_pp_14: 0,
+            text_pp_15: 0,
+            text_retail: 0,
+            text_mu_1: 0,
+            text_mu_2: 0,
+            text_mu_3: 0,
+            text_mu_4: 0
+        }
+    },
+    watch: {
+        text_pp: function() {
+            this.text_pp_12 = Math.floor((this.text_pp * 1.2));
+            this.text_pp_13 = Math.floor((this.text_pp * 1.3));
+            this.text_pp_14 = Math.floor((this.text_pp * 1.4));
+            this.text_pp_15 = Math.floor((this.text_pp * 1.5));
+        },
+        text_retail: function() {
+            this.text_mu_1 = Math.floor((this.text_retail - this.text_pp_12));
+            this.text_mu_2 = Math.floor((this.text_retail - this.text_pp_13));
+            this.text_mu_3 = Math.floor((this.text_retail - this.text_pp_14));
+            this.text_mu_4 = Math.floor((this.text_retail - this.text_pp_15));
+        },
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
+.table-wrapper {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    width: 100%;
+    max-width: 800px;
+}
+.column {
+    display: flex;
+    flex-flow: column nowrap;
+    padding: 0 10px;
+}
+.row {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: right;
+    align-items: center;
+    margin-bottom: 20px;
+}
+.row-text {
+    margin-right: 10px;
+}
+@media only screen and (max-width: 725px) {
+    .column {
+        width: 100%;
+        max-width: 100%;
+    }
+    .row {
+        justify-content: center;
+    }
+    .row-box {
+        /* flex: 1; */
+    }
+    .row-box input {
+        /* width: 100%; */
+    }
+}
+</style>
